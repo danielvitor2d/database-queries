047118ff94fe9bf3726177338006b2df657db58f
danielvitor2d <ripsea.rpg321@gmail.com>
2021-09-25 17:23:39 -0300

    first commit

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -1,4 +1,4 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, ILike, Repository } from 'typeorm';
 
 import { User } from '../../../users/entities/User';
 import { Game } from '../../entities/Game';
@@ -13,18 +13,28 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return await this.repository
+      .createQueryBuilder("games")
+      .where("games.title ilike :title", { title: `%${param}%` })
+      .getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`SELECT count(id) FROM games`);
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    /*
+    return await getRepository(User)
+      .query(`SELECT usr.* FROM users as usr JOIN users_games_games as gme ON usr.id = gme."usersId" WHERE gme."gamesId"='${id}'`);
+    */
+    // /*
+    return await getRepository(User)
+      .createQueryBuilder("users")
+      .innerJoin("users.games", "games")
+      .where("games.id = :id", { id })
+      .select("users")
+      .getMany();
+    // */
   }
 }
